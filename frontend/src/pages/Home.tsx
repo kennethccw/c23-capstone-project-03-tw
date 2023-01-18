96347bf50bd9e27ea6ec3b706ab2aeb97c3941e4
Eric Lam <chungfunlaam44@gmail.com>
2023-01-18 12:00:51 +0800

    upper part carousell UI done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import { useRootSelector } from "../redux/store";
 import { NavBarUtilis } from "../components/NavBarUtilis";
 import styles from "../css/homepage.module.scss";
 import { Bell } from "react-bootstrap-icons";
+import { Carousel } from '@mantine/carousel';
+
 
 export default function Home() {
   const loading = useRootSelector((state: { auth: { loading: any; }; }) => state.auth.loading);
@@ -29,21 +31,29 @@ export default function Home() {
       <div className={styles.upperLayer}>
 
         <div className={styles.logoIconContainer}><img src="photos/logo_pic-09-09.png" className={styles.logoIcon}></img></div>
-        <div className={styles.bellIconContainer}><Bell className={styles.bellIcon}/></div>
+        <div className={styles.bellIconContainer}><Bell className={styles.bellIcon} /></div>
 
       </div>
 
       <div className={styles.lowerPart}>
 
         <div className={styles.userName}>
-          <div >Hi {username} !</div>
+          <div className={styles.greeting}>&nbsp;Hi Jason{username} !</div>
           <button onClick={() => logout()}>logout</button>
         </div>
 
-        <div>
-
+        <div className={styles.carouselPart} >
+          <Carousel mx="auto" withIndicators height={400}>
+            <Carousel.Slide><img src="photos/carousel-02.png" className={styles.carouselPhoto}></img></Carousel.Slide>
+            <Carousel.Slide><img src="photos/寵物美容義工.jpeg" className={styles.carouselPhoto}></img></Carousel.Slide>
+            <Carousel.Slide><img src="photos/義工活動.jpeg" className={styles.carouselPhoto}></img></Carousel.Slide>
+          </Carousel>
         </div>
-
+        
+        <div>
+          <div></div>
+          <div></div>
+        </div>
 
       </div>
 
