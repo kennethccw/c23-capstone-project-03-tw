5d020a4c6f00a06479f0e60123372ac64483f05f
Eric Lam <chungfunlaam44@gmail.com>
2023-01-18 15:29:26 +0800

    to merge main branch

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useRootSelector } from "../redux/store";
 import { NavBarUtilis } from "../components/NavBarUtilis";
 import styles from "../css/homepage.module.scss";
-import { Bell } from "react-bootstrap-icons";
+import { Bell, ChevronRight, Person } from "react-bootstrap-icons";
 import { Carousel } from '@mantine/carousel';
 
 
@@ -43,16 +43,38 @@ export default function Home() {
         </div>
 
         <div className={styles.carouselPart} >
-          <Carousel mx="auto" withIndicators height={400}>
+          <Carousel withIndicators height={300} withControls={false} className={styles.chevron}>
             <Carousel.Slide><img src="photos/carousel-02.png" className={styles.carouselPhoto}></img></Carousel.Slide>
             <Carousel.Slide><img src="photos/寵物美容義工.jpeg" className={styles.carouselPhoto}></img></Carousel.Slide>
             <Carousel.Slide><img src="photos/義工活動.jpeg" className={styles.carouselPhoto}></img></Carousel.Slide>
           </Carousel>
         </div>
-        
-        <div>
-          <div></div>
-          <div></div>
+        <br></br>
+        <div className={styles.socialWorkRecommendContainer}>
+          <div className={styles.socialRecommendBar}>
+            <div className={styles.socialWorkRecommendWord}>&nbsp;&nbsp;社職推介</div>
+            <div className={styles.chevronBarIcon}><ChevronRight /></div>
+          </div>
+
+          <div className={styles.recommendInstance}>
+            <div>
+              <div className={styles.recommendInstancePart}>
+                <Carousel slideSize="70%" height={280} align="start" slideGap="md" controlSize={33} loop withControls={false} withIndicators>
+                  <Carousel.Slide><img src="photos/carousel-02.png" className={styles.recommendInstancePhoto}></img></Carousel.Slide>
+                  <Carousel.Slide><img src="photos/寵物美容義工.jpeg" className={styles.recommendInstancePhoto}></img></Carousel.Slide>
+                  <Carousel.Slide><img src="photos/義工活動.jpeg" className={styles.recommendInstancePhoto}></img></Carousel.Slide>
+                </Carousel>
+              </div>
+              <div><Person /> 剩餘名額 <span>2</span></div>
+            </div>
+            <div>
+              <div>Marketing義工</div>
+              <div>3月4日- 3月5日</div>
+            </div>
+          </div>
+
+
+
         </div>
 
       </div>
