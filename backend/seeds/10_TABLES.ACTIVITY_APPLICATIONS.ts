a30601e66cc9ef6cccf5cd94ca8de9e0b3db2882
Eric Lam <chungfunlaam44@gmail.com>
2023-02-07 18:03:13 +0800

    add several dummy in activity applications table

--- backend/seeds/10_TABLES.ACTIVITY_APPLICATIONS.ts
+++ backend/seeds/10_TABLES.ACTIVITY_APPLICATIONS.ts
@@ -61,6 +61,41 @@ export async function seed(knex: Knex): Promise<void> {
         user_id: 45,
         activity_id: 15,
       },
+      {
+        is_approved: false,
+        is_participated: false,
+        is_cancelled: false,
+        user_id: 2,
+        activity_id: 3,
+      },
+      {
+        is_approved: false,
+        is_participated: false,
+        is_cancelled: false,
+        user_id: 22,
+        activity_id: 3,
+      },
+      {
+        is_approved: false,
+        is_participated: false,
+        is_cancelled: false,
+        user_id: 21,
+        activity_id: 3,
+      },
+      {
+        is_approved: false,
+        is_participated: false,
+        is_cancelled: false,
+        user_id: 19,
+        activity_id: 3,
+      },
+      {
+        is_approved: false,
+        is_participated: false,
+        is_cancelled: false,
+        user_id: 20,
+        activity_id: 3,
+      },
     ]);
     await trx.commit();
   } catch (e) {
