3639da04bdd40365eecd0d9a70b09e1487f5dbc6
Eric Lam <chungfunlaam44@gmail.com>
2023-02-08 17:34:35 +0800

    for git pull

--- frontend/src/components/ApprovedActivityPersonList.tsx
+++ frontend/src/components/ApprovedActivityPersonList.tsx
@@ -6,7 +6,7 @@ export function ApprovedActivityPersonList(props: { activity: ScheduleActivity }
     return (<>
 
         <div className={styles.nameList}>
-            <div className={styles.memberName}>會員：{ }</div>
+            <div className={styles.memberName}>會員：{props.activity.fullname }</div>
             <div className={styles.approvedWord}>申請已經批核</div>
         </div>
 
--- frontend/src/pages/Organisation/OrganisationApproveApplication.tsx
+++ frontend/src/pages/Organisation/OrganisationApproveApplication.tsx
@@ -38,7 +38,7 @@ export default function ApproveApplication() {
   //   return applicationArr;
   // };
   const getPendingAndApprovalApplication = async () => {
-    const approvedApplication = await getApprovedApplication();
+    const approvedApplicationResult = await getApprovedApplication();
     const results = await getPendingApplication();
     const activityIdSet = new Set<number>();
     for (const result of results) {
@@ -57,16 +57,80 @@ export default function ApproveApplication() {
     }
     console.log(applicantArr);
 
-    return { pendingApplication, approvedApplication };
+    /////////// make approvedApplication to be Array of [{ each approved activity detail},[each approved activity person list]]///////// ==> [[{each activity detail},[each activity person list]],[{each activity detail},[each activity person list]],[{each activity detail},[each activity person list]]..........]
+
+
+    const arrayOfAllApprovedActivity: { activity: ScheduleActivity; applicants: { fullname: string; user_id: number }[] }[] = [] = [];
+
+    let arrayOfApprovedActivityID: any = new Set();
+
+    for (let eachApprovedActivity of approvedApplicationResult) {
+      arrayOfApprovedActivityID.add(eachApprovedActivity.activity_id)
+    }
+    console.log(typeof arrayOfApprovedActivityID)
+
+
+
+    for (let eachNumberInArrayOfApprovedActivityID of arrayOfApprovedActivityID) {
+      let approvedApplication = [];
+      let arrayOfPersonList = [];
+      console.log(eachNumberInArrayOfApprovedActivityID)
+
+
+
+
+      arrayOfPersonList.push({
+        fullname: approvedApplicationResult.filter((eachApprovedApplication) => eachApprovedApplication.activity_id === eachNumberInArrayOfApprovedActivityID).map((eachActivity) => eachActivity.user_fullname)[0]!,
+        user_id: approvedApplicationResult.filter((eachApprovedApplication) => eachApprovedApplication.activity_id === eachNumberInArrayOfApprovedActivityID).map((eachActivity) => eachActivity.user_id)[0]!
+      })
+      approvedApplication.push({ activity: approvedApplicationResult.find((eachApprovedApplication) => eachApprovedApplication.activity_id === eachNumberInArrayOfApprovedActivityID)!, applicants: arrayOfPersonList })
+
+
+      console.log(arrayOfPersonList)
+
+      // arrayOfEachApprovedActivityWithPersonList.push(approvedApplication)
+
+      console.log(approvedApplication)   // => now become [[{},[]] ........]
+
+      arrayOfAllApprovedActivity.push(approvedApplication as any)
+    }
+
+
+    console.log(arrayOfAllApprovedActivity)
+console.log(pendingApplication)
+
+
+
+
+
+
+
+
+
+
+
+    return { pendingApplication, arrayOfAllApprovedActivity };
   };
 
   const { isError, data, error, isLoading } = useQuery({
     queryKey: ["organisation/application"],
     queryFn: getPendingAndApprovalApplication,
   });
-  console.log("approvedApplications: ",data?.approvedApplication);
+  console.log("approvedApplications: ", data?.arrayOfAllApprovedActivity);
 
-  console.log("pendingApplications" ,data?.pendingApplication)
+  // console.log(data?.arrayOfAllApprovedActivity.map((each) => each[0]))
+
+
+
+
+
+
+
+
+
+
+
+  console.log("pendingApplications", data?.pendingApplication)
   console.log(data);
   const [isChecked, setIsChecked] = useState(false);
   enum Status {
@@ -159,10 +223,10 @@ export default function ApproveApplication() {
                             // applicantApprovalArr.includes(`${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected`) ||
                             applicantApprovalArr.includes(`${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved`)
                               ? applicantApprovalArr.filter(
-                                  (item) =>
-                                    // item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected` &&
-                                    item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved`
-                                )
+                                (item) =>
+                                  // item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected` &&
+                                  item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved`
+                              )
                               : [...applicantApprovalArr, `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved`]
                           )
                         }
@@ -177,10 +241,10 @@ export default function ApproveApplication() {
                             // applicantApprovalArr.includes(`${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved`) ||
                             applicantApprovalArr.includes(`${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected`)
                               ? applicantApprovalArr.filter(
-                                  (item) =>
-                                    // item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved` &&
-                                    item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected`
-                                )
+                                (item) =>
+                                  // item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-approved` &&
+                                  item !== `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected`
+                              )
                               : [...applicantApprovalArr, `${applicant.fullname}-${applicant.user_id}-${activityObj.activity.activity_id}-rejected`]
                           )
                         }
@@ -204,20 +268,20 @@ export default function ApproveApplication() {
             </>
           ))} */}
 
-        {!data?.approvedApplication.length && status===Status.approved&&(
+        {!data?.arrayOfAllApprovedActivity.length && status === Status.approved && (
           <div className={styles.noApplicationContainer}>
             <h2>暫未有已批核的申請</h2>
           </div>
         )}
 
-        {status === Status.approved && data?.approvedApplication.map((activity, idx) => (
+        {status === Status.approved && data?.arrayOfAllApprovedActivity.map((activity, idx) => (
           <>
-            <ApplicationContainer activity={activity} clickHandler={() => navigate(`/activity/detail?id=${activity.activity_id}&status=approval`)} status={"approved"}/>
+            <ApplicationContainer activity={activity.activity} clickHandler={() => navigate(`/activity/detail?id=${activity.activity.activity_id}&status=approval`)} status={"approved"} />
 
 
 
 
-            {idx !== data.approvedApplication.length - 1 && <hr className={styles.hr90vw} />}
+            {idx !== data.arrayOfAllApprovedActivity.length - 1 && <hr className={styles.hr90vw} />}
           </>
         ))}
 
